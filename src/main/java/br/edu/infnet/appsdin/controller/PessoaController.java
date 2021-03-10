2aad6da726d1bb6bb6dbe62524b263d2b143d354
elberth moraes <elberth.moraes@gmail.com>
2021-03-09 21:51:29 -0300

    JPA e Hibernate

--- src/main/java/br/edu/infnet/appsdin/controller/PessoaController.java
+++ src/main/java/br/edu/infnet/appsdin/controller/PessoaController.java
@@ -2,6 +2,7 @@ package br.edu.infnet.appsdin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.edu.infnet.appsdin.model.negocio.Pessoa;
@@ -19,6 +20,16 @@ public class PessoaController {
 		pessoaService.incluir(pessoa);
 		
 		System.out.println(pessoaService.obterStatus(pessoa));
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping(value = "/pessoa/lista")
+	public String listar() {
+		
+		for(Pessoa p : pessoaService.obterLista()) {
+			System.out.println("Pessoa: " + p.getNome());
+		}
 		
 		return "redirect:/";
 	}
